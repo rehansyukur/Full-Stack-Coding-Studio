16b584d2aab1e54fd017f74fa2c37dcb8f688315
rehansyukur <smkiu.009@gmail.com>
2025-07-31 14:15:25 +0700

    add folder

---
+++ Migration, Seeding, Schema/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+use app\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+    }
+}
